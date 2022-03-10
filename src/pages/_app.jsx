e61fe233b3442be8d9ea15cb89a84e79e06e5e13
Théo <t.lecoublet@gmail.com>
2022-03-10 12:09:42 +0100

    create theming (WIP)

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,8 +1,23 @@
 // import '../styles/globals.css'
 import '../styles/app.scss'
+import ThemeContext, { ThemeContextProvider } from '../context/themeContext/ThemeContext'
+import { useContext } from 'react';
+
+
+const WrappedApp = ({ Component, pageProps }) => {
+  const { isDark } = useContext(ThemeContext);
+  return (
+    <div className={`${isDark ? "theme--dark" : "theme--light"}`}><Component {...pageProps} /></div>
+  );
+};
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <ThemeContextProvider>
+      <WrappedApp {...pageProps} />
+    </ThemeContextProvider>
+  )
 }
 
 export default MyApp
