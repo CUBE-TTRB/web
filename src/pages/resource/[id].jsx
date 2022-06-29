851cbef2e142caf9a03503ba8987ec19d798f21b
Théo <t.lecoublet@gmail.com>
2022-06-30 01:18:22 +0200

    fix context (again)

--- src/pages/resource/[id].jsx
+++ src/pages/resource/[id].jsx
@@ -2,6 +2,9 @@
 import Resource from "../../components/elements/resource/resource"
 import Layout from "../../components/layout/layout"
 import NavigationLayout from "../../components/layout/navigationLayout"
+import ResourceService from "../../service/resource.service"
+
+
 export default function resourceId() {
   return (
     <>
@@ -10,6 +13,16 @@ export default function resourceId() {
   )
 }
 
+export async function getServerSideProps({params}) {
+ 
+  let post = await ResourceService.getRessource(true);
+
+  return {
+    props: {
+        post
+    }, // will be passed to the page component as props
+  }
+}
 
 resourceId.getLayout = function getLayout(page) {
   return (
