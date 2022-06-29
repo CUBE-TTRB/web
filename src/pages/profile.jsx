6ee9b768a067b8553676f3441447ed80458c87c4
Théo <t.lecoublet@gmail.com>
2022-06-29 10:14:14 +0200

    add docker

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -4,18 +4,22 @@ import UserProfile from '../components/elements/userProfile/userProfile'
 import Layout from '../components/layout/layout'
 import NavigationLayout from '../components/layout/navigationLayout'
 import { AppContext } from '../context/state';
-import { useContext } from 'react'
-import Router from 'next/router'
+import { useContext, useEffect } from 'react'
+import { useRouter } from 'next/router'
+
 
 export default function Profil() {
   const authContext = useContext(AppContext);
+  const router = useRouter();
+  useEffect(() => {
+    if(!(authContext.state.auth))router.push("/");
+  }, [])
   if(authContext.state.auth){
-    return (
-      <UserProfile />
-    )
+    return (<UserProfile />)
   }else {
-    Router.push("/")
+    return null
   }
+
 
 }
 
